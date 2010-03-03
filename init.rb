9588bf9d0fdac74ff989581c34bd7fa5d1781433
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-02 17:09:52 -0800

    [#3646] Added a configuration panel to configing the statuses and projects

--- init.rb
+++ init.rb
@@ -7,4 +7,12 @@ Redmine::Plugin.register :redmine_restricted_status do
   author_url 'http://www.littlestreamsoftware.com'
   description 'Plugin to restrict status changes in specific projects.'
   version '0.1.0'
+
+
+  settings(:partial => 'settings/restricted_status_settings',
+           :default => {
+             'restricted_projects' => [],
+             'allowed_statuses' => []
+           })
+  
 end
