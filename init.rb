5d65a8c2676c5039e8998b8e8b5bdf9cc70e9694
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-02 16:33:08 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_restricted_status do
+  name 'Redmine Restricted Status plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
