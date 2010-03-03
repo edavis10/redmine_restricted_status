ae7de5d5f78fe628f3922dd16415fbfdb60a3473
Eric Davis <edavis@littlestreamsoftware.com>
2010-03-02 16:40:18 -0800

    Update init

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_restricted_status do
-  name 'Redmine Restricted Status plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Restricted Status'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'Plugin to restrict status changes in specific projects.'
+  version '0.1.0'
 end
